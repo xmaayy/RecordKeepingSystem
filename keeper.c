60e1d46780b63b0cc837b032af084e2b0e96b7e5
JoshuaSFryer <josh.s.fryer@gmail.com>
2019-03-25 17:02:18 -0400

    Fix issues with implicit function declarations/function order in file

--- keeper.c
+++ keeper.c
@@ -40,17 +40,15 @@ int array_length(Record* r) {
     return (int) sizeof(r)/sizeof(Record);
 }
 
-// Look for a message in the queue that contains a record. If there is one,
-// fetch it and add the record it contains to the database.
-int add_record() {
-    record_message msg;
-    int len = msgrcv(admin_queue, &msg, sizeof(Record), DATA, 0);
-    Record data = msg.record;
-    // This is super inefficient and redundant but meh.
-    // We need to get this data copied somewhere permanent, this is a simple
-    // dumb solution.
-    insert_record(create_record(data.name, data.deptName, data.employeeNum, 
-                                data.salary));
+
+// Create a new record and return a pointer to it.
+Record* create_record(char* name, char* dept, int id, int salary) {
+    Record* r = malloc((size_t) sizeof(Record*));
+    strcpy(r->name, name);
+    strcpy(r->deptName, dept);
+    r->employeeNum = id;
+    r->salary = salary;
+    return r;
 }
 
 // Place a new record into the database.
@@ -66,14 +64,17 @@ int insert_record(Record* r) {
     }
 }
 
-// Create a new record and return a pointer to it.
-Record* create_record(char* name, char* dept, int id, int salary) {
-    Record* r = malloc((size_t) sizeof(Record*));
-    strcpy(r->name, name);
-    strcpy(r->deptName, dept);
-    r->employeeNum = id;
-    r->salary = salary;
-    return r;
+// Look for a message in the queue that contains a record. If there is one,
+// fetch it and add the record it contains to the database.
+int add_record() {
+    record_message msg;
+    int len = msgrcv(admin_queue, &msg, sizeof(Record), DATA, 0);
+    Record data = msg.record;
+    // This is super inefficient and redundant but meh.
+    // We need to get this data copied somewhere permanent, this is a simple
+    // dumb solution.
+    insert_record(create_record(data.name, data.deptName, data.employeeNum, 
+                                data.salary));
 }
 
 // Fetch a record by its employee ID.
